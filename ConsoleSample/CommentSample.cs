d37e627ec55f6c2fb990f457ba016e0689ee1539
Ryo Watanabe <ryo33345@gmail.com>
2021-01-26 18:43:33 +0900

    コメントのサンプル

---
+++ ConsoleSample/CommentSample.cs
@@ -0,0 +1,21 @@
+namespace ConsoleTest {
+
+  class CommentSample {
+
+        /// <summary>
+        /// ドキュメンテーションコメント
+        /// 関数などの説明を書く
+        /// </summary>
+        public static void Run() {
+            // 行コメント
+            // コメントアウトされたコードは実行されない
+            // System.Console.WriteLine("AAAAAAAA");
+
+            /*
+              範囲コメント
+              コメントアウトされたコードは実行されない
+              System.Console.WriteLine("AAAAAAAA");
+            */
+        }
+  }
+}
--- ConsoleSample/Program.cs
+++ ConsoleSample/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿// 名前空間を利用する
+using System;
 
-namespace ConsoleSample
+// 名前空間に含める
+namespace ConsoleTest
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            CommentSample.Run();
+
+            // コンソール出力
             Console.WriteLine("Your Name? ");
             var name = Console.ReadLine();
 
+            // コンソール入力
             Console.WriteLine("Hello, {0}", name);
+
         }
+
+        
     }
 }
